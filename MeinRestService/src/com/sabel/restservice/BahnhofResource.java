1bc250107cd8064e7f3dead2033761389bb04578
m.strohmayer <matthias.strohmayer@web.de>
2017-05-24 11:06:39 +0200

    InitalCommit

---
+++ MeinRestService/src/com/sabel/restservice/BahnhofResource.java
@@ -0,0 +1,65 @@
+package com.sabel.restservice;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/haltestelle")
+public class BahnhofResource {
+	
+	private BahnhofService bs = new BahnhofService(); 
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/ping")
+	public String ping(){
+		return "Die aktuelle Serverzeit ist: " + new Date().toString();		
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	@Path("/{id}")
+	public Bahnhof getBahnhof(@PathParam("id") int id){
+		return bs.find(id);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Bahnhof> getAllBahnhof(){
+		return bs.receiveAll();
+	}
+	
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Path("/new")
+	public String createBahnhof(
+			@FormParam("name") String name,
+			@FormParam("lon") double lon,
+			@FormParam("lat") double lat){
+		Bahnhof bh = new Bahnhof();
+		bh.setName(name);
+		bh.setLat(lat);
+		bh.setLon(lon);
+		bs.save(bh);		
+		return "<html><head></head><body><p>Erfolgreich hinzugefügt</p></br>" + bh.toString() + "</body></html>";
+	}
+	
+	@DELETE
+	@Produces(MediaType.TEXT_HTML)
+	@Path("/{id}")
+	public String deleteBahnhof(@PathParam("id") int id){
+		bs.remove(id);
+		return "<html><head></head><body><p>" + id + "Erfolgreich gelöscht</p></body></html>";
+	}
+
+}
---
+++ MeinRestService/src/com/sabel/restservice/BahnhofService.java
@@ -0,0 +1,56 @@
+package com.sabel.restservice;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import org.eclipse.persistence.exceptions.i18n.EntityManagerSetupExceptionResource;
+
+public class BahnhofService {
+	private EntityManagerFactory emf;
+	private EntityManager em;
+
+	public BahnhofService(){
+		emf = Persistence.createEntityManagerFactory("bahnhof");
+		em = emf.createEntityManager();
+	}
+	
+	public void save(Bahnhof bh){
+		em.getTransaction().begin();
+		em.persist(bh);
+		em.getTransaction().commit();
+	}
+	
+	public Bahnhof remove(int id){
+		Bahnhof bh = find(id);
+		em.getTransaction().begin();
+		em.remove(bh);
+		em.getTransaction().commit();
+		return bh;
+	}
+	
+	public Bahnhof find(int id){
+		return em.find(Bahnhof.class, id);
+	}
+	
+	public List<Bahnhof> receiveAll(){
+		TypedQuery<Bahnhof> query = em.createQuery("Select h from Bahnhof h ",Bahnhof.class);
+		return query.getResultList();
+	}
+	
+	public void close(){
+		if (em != null){
+			em.close();
+			em = null;
+		}
+		if (emf != null){
+			emf.close();
+			em = null;
+		}
+	}
+}
+
+
